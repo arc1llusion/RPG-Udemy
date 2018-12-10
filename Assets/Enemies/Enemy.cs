f5d82cf6c137897fc6b4449c64fd70a2c78e17c5
Jared Germano <jgermano@cadlearning.com>
2018-12-10 15:15:21 -0500

    Up to lec 45

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 5f;
+    [SerializeField] float attackStopRadius = 5f;
+
+    AICharacterControl aiCharacterControl;
+    ThirdPersonCharacter thirdPersonCharacter = null;
+    GameObject player = null;
 
     float currentHealthPoints = 100f;
 
@@ -13,6 +20,26 @@ public class Enemy : MonoBehaviour {
         get
         {
             return currentHealthPoints / maxHealthPoints;
+        }
+    }
+
+    public void Start()
+    {
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+        aiCharacterControl = GetComponent<AICharacterControl>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if(distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
         }
     }
 }
