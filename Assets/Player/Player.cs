b053d133229b71fea24a16cf61e392e6acad1281
Jared Germano <jgermano@cadlearning.com>
2018-12-13 20:20:45 -0500

    update

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IDamageable {
 
+    [SerializeField] int enemyLayer = 9;
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float damagePerHit = 10f;
+    [SerializeField] float minTimeBetweenHits = .5f;
+    [SerializeField] float maxAttackRange = 2f;
 
-    float currentHealthPoints = 100f;
+    GameObject currentTarget;
+    float currentHealthPoints;
+    CameraRaycaster cameraRaycaster;
+    float lastHitTime = 0f;
 
-    public float healthAsPercentage
+    public float healthAsPercentage { get { return currentHealthPoints / maxHealthPoints; }}
+
+    void Start()
     {
-        get
+        cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+        cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+        currentHealthPoints = maxHealthPoints;
+    }
+
+    void OnMouseClick(RaycastHit raycastHit, int layerHit)
+    {
+        if (layerHit == enemyLayer)
         {
-            return currentHealthPoints / maxHealthPoints;
+            var enemy = raycastHit.collider.gameObject;
+             
+            // Check enemy is in range
+            if ((enemy.transform.position - transform.position).magnitude > maxAttackRange)
+            {
+                return;
+            }
+
+            currentTarget = enemy;
+
+            var enemyComponent = enemy.GetComponent<Enemy>();
+            if (Time.time - lastHitTime > minTimeBetweenHits)
+            {
+                enemyComponent.TakeDamage(damagePerHit);
+                lastHitTime = Time.time;
+            }
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
     }
 }
