1d3d16ff8c4d18237b37ec2d3f784d7a3b8d92c9
Arc1llusion <arc1llusion@gmail.com>
2019-01-06 09:54:04 -0500

    Lecture 179

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -36,21 +36,26 @@ namespace RPG.Characters
             WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
             currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
 
-            if (distanceToPlayer > chaseRadius && state != State.patrolling)
+            bool inWeaponCircle = distanceToPlayer <= currentWeaponRange;
+            bool inChaseCircle = distanceToPlayer <= chaseRadius && distanceToPlayer > currentWeaponRange;
+            bool outsideChaseRing = distanceToPlayer > chaseRadius;
+
+            if (outsideChaseRing)
             {
                 StopAllCoroutines();
                 weaponSystem.StopAttacking();
                 StartCoroutine(Patrol());
             }
-            if (distanceToPlayer <= chaseRadius && state != State.chasing)
+            if (inChaseCircle)
             {
                 StopAllCoroutines();
                 weaponSystem.StopAttacking();
                 StartCoroutine(ChasePlayer());
             }
-            if (distanceToPlayer <= currentWeaponRange && state != State.attacking)
+            if (inWeaponCircle)
             {
                 StopAllCoroutines();
+                state = State.attacking;
                 weaponSystem.AttackTarget(player.gameObject);
             }
         }
